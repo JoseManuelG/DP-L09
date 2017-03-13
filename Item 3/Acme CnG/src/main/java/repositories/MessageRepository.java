55cd10e30dc1aab4b5da8ec9a3053cf087e73c14
Roldans <roldis2@hotmail.com>
2017-03-13 10:28:17 +0100

    Hechas las queries de repository de mensajeria

--- Item 3/Acme CnG/src/main/java/repositories/MessageRepository.java
+++ Item 3/Acme CnG/src/main/java/repositories/MessageRepository.java
@@ -1,6 +1,9 @@
 package repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Message;
@@ -10,4 +13,13 @@ public interface MessageRepository extends JpaRepository<Message, Integer> {
 
 	
 	//TODO Tengo que poner querys para encontrar los mensajes enviados y recibidos de una persona
+	
+	
+	//Find all the sended messages for a given actor
+	@Query("select m from Message m where m.sender_id=?1 & isCopy=false")
+	public List<Message> findSendedMessageOfActor(int senderId);
+
+	//Find all the received messages for a given actor
+	@Query("select m from Message m where m.recipient_id=?1 & isCopy=true")
+	public List<Message> findReceivedMessageOfActor(int recipientId);
 }
--- Item 3/Acme CnG/src/main/java/services/MessageService.java
+++ Item 3/Acme CnG/src/main/java/services/MessageService.java
@@ -1,8 +1,11 @@
 package services;
 
 import java.util.Collection;
+import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
@@ -20,22 +23,22 @@ public class MessageService {
 		private MessageRepository	messageRepository;
 		
 		//Supported Services--------------------------------------------------------------------
-
+		@Autowired
+		private Actorservice actorService;
 		
 		
 		//Simple CRUD methods------------------------------------------------------------
-		public Message create(Actor sender, Actor recipient){
+		public Message create(Actor recipient){
 			Message result = new Message();
 			
 			result.setRecipient(recipient);
 			result.setRecipientName(recipient.getName());
-
+			Actor sender = actorService.findByPrincipal();
 			result.setSender(sender);
 			result.setSenderName(sender.getName());
 			
 			
-			//TODO Falta los attachments habria que settearlo
-			//result.setAttachments(new Collection<Attachments>());
+			
 			//TODO Falta el booleano isCopy
 			//result.setIsCopy(false);
 			return result;
@@ -60,14 +63,57 @@ public class MessageService {
 		
 		
 		
-		public Message Save(Message message){
+		public Message save(Message message){
 			Message result;
+			Assert.isNull(message.getRecipient(),"El mensaje debe tener un destinatario");
+			Assert.isNull(message.getRecipient().getName(),"El mensaje debe tener el nombre del destinatario");
+			Assert.hasText(message.getRecipient().getName(),"El mensaje debe tener el nombre del destinatario");
+			Assert.isNull(message.getSender(),"El mensaje debe tener un remitente");
+			Assert.hasText(message.getSender().getName(),"El mensaje debe tener el nombre del remitente");
+
+			Assert.hasText(message.getText(), "El mensaje debe tener un cuerpo");
+			Assert.hasText(message.getTitle(), "El mensaje debe tener un titulo");
+
+		//	Assert.isNull(message.getSendingMoment(), "El mensaje debe tener la fecha de envio");
 			
 			/*TODO
 			 * Creamos copia del mensaje en un segundo mensaje poniendole el isCopy a true y guardariamos ambos;
 			 * 
 			 */
+			
+			message.setSendingMoment(new Date(System.currentTimeMillis()-1000));
+			Message copyMessage = copyMessage(message);
 			result=messageRepository.save(message);
+			messageRepository.save(copyMessage);
 			return result;
 		}
+		
+		
+		private Message copyMessage(Message message){
+			Message result = new Message();
+			
+			result.setRecipient(message.getRecipient());
+			result.setRecipientName(message.getRecipient().getName());
+
+			result.setSender(message.getSender());
+			result.setSenderName(message.getSender().getName());
+			result.setText(message.getText());
+			result.setTitle(message.getTitle());
+			result.setSendingMoment(message.getSendingMoment());
+			
+			
+			//result.setIsCopy(true);
+			return result;
+		}
+		//Devuelve los mensajes que ha enviado el actor
+		public List<Message> findSendedMessageOfActor(int senderId){
+			List<Message> result = messageRepository.findSendedMessageOfActor(senderId);
+			return result;
+		}
+
+		//Devuelve los mensajes que ha recivido el actor	
+		public List<Message> findReceivedMessageOfActor(int recipientId){
+			List<Message> result = messageRepository.findReceivedMessageOfActor(recipientId);
+			return result;
+	}
 }
