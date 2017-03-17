fc92fa393ddeabb01f458bc5b0575c030335f501
Javi <trarsheise@gmail.com>
2017-03-17 12:34:19 +0100

    Mostrando banner en la pagina de welcome

--- Item 3/Acme CnG/src/main/java/controllers/WelcomeController.java
+++ Item 3/Acme CnG/src/main/java/controllers/WelcomeController.java
@@ -13,14 +13,23 @@ package controllers;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import services.ConfigurationService;
+import domain.Configuration;
 
 @Controller
 @RequestMapping("/welcome")
 public class WelcomeController extends AbstractController {
+
+	// Services ------------------------------------------------------------------
+
+	@Autowired
+	private ConfigurationService	configurationService;
+
 
 	// Constructors -----------------------------------------------------------
 
@@ -31,17 +40,19 @@ public class WelcomeController extends AbstractController {
 	// Index ------------------------------------------------------------------		
 
 	@RequestMapping(value = "/index")
-	public ModelAndView index(@RequestParam(required = false, defaultValue = "John Doe") final String name) {
+	public ModelAndView index() {
 		ModelAndView result;
-		SimpleDateFormat formatter;
+		final SimpleDateFormat formatter;
 		String moment;
+		Configuration configuration;
 
+		configuration = this.configurationService.findOne();
 		formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		moment = formatter.format(new Date());
 
 		result = new ModelAndView("welcome/index");
-		result.addObject("name", name);
 		result.addObject("moment", moment);
+		result.addObject("banner", configuration.getBanner());
 
 		return result;
 	}
