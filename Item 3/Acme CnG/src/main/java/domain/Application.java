399339ec07a826a6740ce74895165e7901485963
Roldans <roldis2@hotmail.com>
2017-03-24 10:00:34 +0100

    Cambios para el B+ tree

--- Item 3/Acme CnG/src/main/java/domain/Application.java
+++ Item 3/Acme CnG/src/main/java/domain/Application.java
@@ -4,13 +4,18 @@ package domain;
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 @Entity
 @Access(AccessType.PROPERTY)
+@Table(indexes = {
+	@Index(columnList = "status")
+})
 public class Application extends DomainEntity {
 
 	// Attributes -------------------------------------------------------------
